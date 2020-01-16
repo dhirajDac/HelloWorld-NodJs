47fbdb6236456e09c207eff5495c0712a6315056
Dhiraj Kumar <dhiraj.sk@gmail.com>
2020-01-16 10:39:06 +0530

    Plain calculator.js fo nodejs

---
+++ Calculator.js
@@ -0,0 +1,23 @@
+var calculator={
+     add(x,y)
+{
+    return x+y;
+},
+ substract(x,y)
+{
+    return x-y;
+},
+ multiply(x,y)
+{
+    return x*y;
+},
+ divide(x,y)
+{
+    return x/y;
+}
+};
+var x=100,y=50;
+console.log(calculator.add(x,y));
+console.log(calculator.substract(x,y));
+console.log(calculator.multiply(x,y));
+console.log(calculator.substract(x,y));
